3848fb43b9d3af12a23946ca3d88f5897955f7c4
302A-01\cre <a>
2025-10-07 12:03:52 +0900

    todoリスト終了！

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,7 +13,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return view('tasks.index');
+        $tasks = Task::where('status', false)->get();
+
+        return view('tasks.index', compact('tasks'));
     }
 
     /**
@@ -64,7 +66,8 @@ class TaskController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $task = Task::find($id);
+        return view('tasks.edit', compact('task'));
     }
 
     /**
@@ -72,7 +75,41 @@ class TaskController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        //「編集する」ボタンをおしたとき
+        if ($request->status === null) {
+            $rules = [
+                'task_name' => 'required|max:100',
+            ];
+
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+
+            Validator::make($request->all(), $rules, $messages)->validate();
+
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->name = $request->input('task_name');
+
+            //データベースに保存
+            $task->save();
+        } else {
+            //「完了」ボタンを押したとき
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; //true:完了、false:未完了
+
+            //データベースに保存
+            $task->save();
+        }
+
+
+        //リダイレクト
+        return redirect('/tasks');
     }
 
     /**
@@ -80,6 +117,8 @@ class TaskController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        Task::find($id)->delete();
+
+        return redirect('/tasks');
     }
 }
