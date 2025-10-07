ff615a87236258a556a17fbd9bebd90b52e19bff
302A-01\cre <a>
2025-10-07 11:41:56 +0900

    Todoリスト作成できて、１００文字以内や必要項目です、などの条件を入れることができた！フレームワークはプラモ！！

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task; //追加
+use Illuminate\Support\Facades\Validator; //追加２
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('tasks.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'task_name' => 'required|max:100',
+        ];
+
+        $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+
+        Validator::make($request->all(), $rules, $messages)->validate();
+
+
+        //モデルをインスタンス化
+        $task = new Task;
+
+        //モデル->カラム名 = 値 で、データを割り当てる
+        $task->name = $request->input('task_name');
+
+        //データベースに保存
+        $task->save();
+
+        //リダイレクト
+        return redirect('/tasks');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
